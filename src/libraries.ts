671352297654c5a0e4eff6afcfc9006a0f73538c
Ferran Bals - iMac <ferranbals@gmail.com>
2024-05-26 11:15:24 +0200

    Post a library endpoint added - initial version

--- src/libraries.ts
+++ src/libraries.ts
@@ -5,11 +5,14 @@ const librariesRouter = Router();
 
 librariesRouter.get("/", async (req, res) => {
   try {
+    const totalLibraries = await prisma.library.count();
     const libraries = await prisma.library.findMany({
       orderBy: { libraryId: "asc" },
       select: { libraryId: true, name: true, city: true },
     });
-    res.status(200).json({ libraries });
+    res
+      .status(200)
+      .json({ "total libraries": totalLibraries, libraries });
   } catch (e) {
     res.status(500).json({ error: "Internal server error" });
   }
@@ -17,8 +20,26 @@ librariesRouter.get("/", async (req, res) => {
 
 //get library by id
 
-//get library by id
+//get library by name
 
 //introduce a library
+librariesRouter.post("/", async (req, res) => {
+  try {
+    const { name, city, address } = req.body;
+    if (name === undefined || typeof name !== "string") {
+      return res.status(400).json({ error: "Missing name field" });
+    }
+    const library = await prisma.library.create({
+      data: { name, city, address },
+    });
+    res.status(201).json({
+      message: `Id de la biblioteca introducida: ${library.libraryId}`,
+      content: library,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 export default librariesRouter;
