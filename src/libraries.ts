9912bc84e3315f73c498371b8fb8a455f6a00ea2
Ferran Bals - iMac <ferranbals@gmail.com>
2024-05-26 12:43:13 +0200

    Added response.ts + improve response management of library endpoints

--- src/libraries.ts
+++ src/libraries.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { prisma } from "./db";
+import { send } from "./response";
 
 const librariesRouter = Router();
 
@@ -10,11 +11,12 @@ librariesRouter.get("/", async (req, res) => {
       orderBy: { libraryId: "asc" },
       select: { libraryId: true, name: true, city: true },
     });
-    res
-      .status(200)
-      .json({ "total libraries": totalLibraries, libraries });
+    send(res).ok({
+      msg: `Total de bibliotecas: ${totalLibraries}`,
+      libraries,
+    });
   } catch (e) {
-    res.status(500).json({ error: "Internal server error" });
+    send(res).internalError("Could not get the libraries");
   }
 });
 
@@ -26,38 +28,34 @@ librariesRouter.get("/:id", async (req, res) => {
       where: { libraryId: Number(id) },
       select: { libraryId: true, name: true, city: true },
     });
-    res.status(200).json({ library });
+    send(res).ok(library);
   } catch (e: any) {
     if (e.name === "NotFoundError") {
-      return res.status(404).json({ message: "Not found" });
+      return send(res).notFound();
     }
-    res.status(500).json({
-      name: e.name,
-      message: e.message,
-      error: e.toString(),
-    });
+    send(res).internalError("Could not create the library.");
   }
 });
 
 //get library by name
+//TODO
 
 //introduce a library
 librariesRouter.post("/", async (req, res) => {
   try {
     const { name, city, address } = req.body;
     if (name === undefined || typeof name !== "string") {
-      return res.status(400).json({ error: "Missing name field" });
+      send(res).badRequest("Missing name field");
     }
     const library = await prisma.library.create({
       data: { name, city, address },
     });
-    res.status(201).json({
-      message: `Id de la biblioteca introducida: ${library.libraryId}`,
-      content: library,
+    send(res).createdOk({
+      msg: `Id de la biblioteca introducida: ${library.libraryId}`,
+      library,
     });
   } catch (e) {
-    console.log(e);
-    res.status(500).json({ error: "Internal server error" });
+    send(res).internalError("Could not post the library");
   }
 });
 
